3e164d9c1f9c52650f68e681b7a7cb51f8e91f19
shfhshddhd <onebhaiabcd@gmail.com>
2026-09-24 22:31:38 +0530

    Update source URL in buttons.go

--- kanha/core/buttons.go
+++ kanha/core/buttons.go
@@ -274,14 +274,14 @@ func GetStartMarkup(chatID int64) td.ReplyMarkup {
 				UrlBtn(F(chatID, "SUPPORT_BTN"), config.SupportChat),
 			},
 			{
-				UrlBtn(F(chatID, "SOURCE_BTN"), "https://github.com/Oyekanhaa/KanhaMusic"),
+				UrlBtn(F(chatID, "SOURCE_BTN"), "https://github.com/shfhshddhd/Vv.git"),
 			},
 		},
 	}
 }
 
 func GetRepoMarkup(chatID int64, devURL string) td.ReplyMarkup {
-	repoURL := "https://github.com/Oyekanhaa/KanhaMusic"
+	repoURL := "https://github.com/shfhshddhd/Vv.git"
 	if devURL == "" {
 		if config.DevURL != "" {
 			devURL = config.DevURL
